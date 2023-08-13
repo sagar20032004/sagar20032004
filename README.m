0c8366d83d89c1481650a3dc55e6b28cf807fc63
sagar20032004 <142142934+sagar20032004@users.noreply.github.com>
2023-08-13 13:35:32 +0530

    Create README.m
    Wiwiw

---
+++ README.m
@@ -0,0 +1,34 @@
+╔══════════════════════════════════════════════╗
+║                  Daily Study Timetable                  ║
+╠══════════════════════════════════════════════╣
+║ **Morning:**                                          ║
+║ - 8:00 AM - 9:30 AM: Morning Routine, Breakfast, Prep ║
+║ - 9:30 AM - 11:00 AM: Study Session 1                      ║
+║                                                      ║
+║ **Mid-Morning:**                                    ║
+║ - 11:15 AM - 12:45 PM: Study Session 2                   ║
+║                                                      ║
+║ **Lunch Break:**                                    ║
+║ - 12:45 PM - 1:45 PM: Lunch, Short Break            ║
+║                                                      ║
+║ **Afternoon:**                                      ║
+║ - 1:45 PM - 3:15 PM: Study Session 3                    ║
+║                                                      ║
+║ **Mid-Afternoon:**                                  ║
+║ - 3:30 PM - 5:00 PM: Study Session 4                    ║
+║                                                      ║
+║ **Evening Break:**                                  ║
+║ - 5:00 PM - 5:30 PM: Refreshment, Break           ║
+║                                                      ║
+║ **Evening:**                                        ║
+║ - 5:30 PM - 7:00 PM: Study Session 5                    ║
+║                                                      ║
+║ **Dinner Break:**                                  ║
+║ - 7:00 PM - 8:00 PM: Dinner, Relaxation         ║
+║                                                      ║
+║ **Night:**                                          ║
+║ - 8:00 PM - 9:30 PM: Study Session 6                    ║
+║                                                      ║
+║ **Pre-Bedtime:**                                    ║
+║ - 9:45 PM - 10:15 PM: Review, Prepare Next Day   ║
+╚══════════════════════════════════════════════╝
